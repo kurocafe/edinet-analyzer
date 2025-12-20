dfb0b3586d32317b754adee03c2eb14e8c5abe1c
fantastic <kotatoyoda@icloud.com>
2025-12-21 01:09:03 +0900

    add table for each growth ranking

--- frontend/pages/ranking/index.vue
+++ frontend/pages/ranking/index.vue
@@ -2,16 +2,19 @@
   <div>
     all graphs should be here!!
     <h1>Revenues: </h1>
+    <Table :rankingdata="revenues" />
     <pre>
       {{ JSON.stringify(revenues, null, 3) }}
     </pre>
 
     <h1>Profits: </h1>
+    <Table :rankingdata="profits" />
     <pre>
       {{ JSON.stringify(profits, null, 3) }}
     </pre>
 
     <h1>Incomes:</h1>
+    <Table :rankingdata="incomes" />
     <pre>
       {{ JSON.stringify(incomes, null, 3) }}
     </pre>
@@ -19,6 +22,8 @@
 </template>
 
 <script setup>
+import Table from '~/components/Ranking/Table.vue';
+
   const {
     revenues, 
     profits, 
