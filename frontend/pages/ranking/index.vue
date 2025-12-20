c0cc2b7b0dca34ed03c923ac568fa543fc76190f
fantastic <kotatoyoda@icloud.com>
2025-12-21 00:42:40 +0900

    create api endpoint for revenue, profit, income ranking

--- frontend/pages/ranking/index.vue
+++ frontend/pages/ranking/index.vue
@@ -1,7 +1,20 @@
 <template>
   <div>
     all graphs should be here!!
-    {{ revenues }}
+    <h1>Revenues: </h1>
+    <pre>
+      {{ JSON.stringify(revenues, null, 3) }}
+    </pre>
+
+    <h1>Profits: </h1>
+    <pre>
+      {{ JSON.stringify(profits, null, 3) }}
+    </pre>
+
+    <h1>Incomes:</h1>
+    <pre>
+      {{ JSON.stringify(incomes, null, 3) }}
+    </pre>
   </div>
 </template>
 
